c84eca6eab3a1ec20d40133d294ae3944027b905
NostalgiaBox <rfenster@gmail.com>
2020-01-09 00:22:55 -0500

    Added in optional Features

--- js/script.js
+++ js/script.js
@@ -11,9 +11,11 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 var quotes = [
-  { quote: "I never said that.", source: "Mark Twain", citation: "My Book", year: '1950'},
-  { quote: "Live Long and Prosper", source: "Mr. Spock", citiation: "Star Trek", year: 'Stardate 4032'},
-  {quote: "Yadda Yadda Yadda", source: "Jerry Seinfeld", year: "1991"}
+  { quote: "I have been studying the traits and dispositions of the 'lower animals' (so called) and contrasting them with the traits and dispositions of man. I find the result humiliating to me.", source: "Mark Twain", citation: "Letters From Earth", tags: ['#TwainMan', '#FreightTwain']},
+  { quote: 'Go so far away that you stop being afraid of not coming back.', source: 'Eve Ensler', citation: 'I Am an Emotional Creature: The Secret Life of Girls Around the World', year: 2010},
+  { quote: "Live Long and Prosper", source: "Mr. Spock", citiation: "Star Trek", year: '1967', tags: ['#Trekkies', '#Vulcan', '#Space']},
+  { quote: "Yadda Yadda Yadda", source: "Jerry Seinfeld", year: "1997", tags: ['#Yadda', '#NoSoupForYou']},
+  { quote: 'We are what we pretend to be, so we must be careful about what we pretend to be.', source: 'Kurt Vonnegut', citation: 'Mother Night', tags: ['#KeepingIt100']}
 ]
 console.log(quotes);
 /***
@@ -32,7 +34,12 @@ function printQuote(){
   var currentQuote = getRandomQuote();
   var html = ''
   var quoteDiv = document.getElementById('quote-box')
-  html += '<p class="quote">' + currentQuote.quote + '</p>'
+  html += '<p class="quote">' + currentQuote.quote 
+  if ('tags' in currentQuote){
+    
+      html += '  ' + currentQuote.tags.join(', ')
+  }
+  html += '</p>'
   html += '<p class="source">' + currentQuote.source 
   if ('citation' in currentQuote) {
     html += '<span class="citation">' + currentQuote.citation + '</span>'
@@ -42,8 +49,26 @@ function printQuote(){
   }
   html += '</p>'
   quoteDiv.innerHTML = html
+  setRandomBackground()
+  
 }
 
+function setRandomBackground(){
+  var randomNumber = Math.floor(Math.random() * 6)
+  if (randomNumber == 0){
+    document.body.style.background = 'black';
+  }else if (randomNumber == 1){
+    document.body.style.background = 'blue';
+  }else if (randomNumber == 2){
+    document.body.style.background = 'green';
+  }else if (randomNumber == 3){
+    document.body.style.background = 'red';
+  }else if (randomNumber == 4){
+    document.body.style.background = 'purple';
+  }else if (randomNumber == 5){
+    document.body.style.background = 'yellow';
+  }
+}
 
 /***
  * click event listener for the print quote button
