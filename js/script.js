52dc05cfa24a229ae28decd3a54e85d382fe4949
NostalgiaBox <rfenster@gmail.com>
2020-01-09 00:03:19 -0500

    Added in base functionality

--- js/script.js
+++ js/script.js
@@ -10,19 +10,39 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
-
+var quotes = [
+  { quote: "I never said that.", source: "Mark Twain", citation: "My Book", year: '1950'},
+  { quote: "Live Long and Prosper", source: "Mr. Spock", citiation: "Star Trek", year: 'Stardate 4032'},
+  {quote: "Yadda Yadda Yadda", source: "Jerry Seinfeld", year: "1991"}
+]
+console.log(quotes);
 /***
  * `getRandomQuote` function
 ***/
+function getRandomQuote() {
+  var random = Math.floor(Math.random() * quotes.length);
+  return(quotes[random ]);
+}
 
-
-
+getRandomQuote();
 /***
  * `printQuote` function
 ***/
-
+function printQuote(){
+  var currentQuote = getRandomQuote();
+  var html = ''
+  var quoteDiv = document.getElementById('quote-box')
+  html += '<p class="quote">' + currentQuote.quote + '</p>'
+  html += '<p class="source">' + currentQuote.source 
+  if ('citation' in currentQuote) {
+    html += '<span class="citation">' + currentQuote.citation + '</span>'
+  }
+  if ('year' in currentQuote){
+    html += '<span class="year">' + currentQuote.year + '</span>'
+  }
+  html += '</p>'
+  quoteDiv.innerHTML = html
+}
 
 
 /***
